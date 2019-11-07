d2432a20baaa6129ad9ca571df6fb544331f8de0
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-11-07 13:09:59 +0100

    start hp and fixed distance

--- training.py
+++ training.py
@@ -12,7 +12,14 @@ import views
 
 import matplotlib.pyplot as plt
 
-def score_kernel(kernel, properties, idxs_train, idxs_test):
+
+def score_kernels(kernels, properties, idxs_train, idxs_test, l2reg=1e-6):
+
+
+    return
+
+
+def score_kernel(kernel, properties, idxs_train, idxs_test, l2reg=1e-6):
 
     kernel_train = copy.deepcopy(kernel[np.ix_(idxs_train, idxs_train)])
     kernel_test  = copy.deepcopy(kernel[np.ix_(idxs_test, idxs_train)])
@@ -20,7 +27,7 @@ def score_kernel(kernel, properties, idxs_train, idxs_test):
     properties_train = properties[idxs_train]
     properties_test = properties[idxs_test]
 
-    alpha = qml.math.cho_solve(kernel_train, properties_train, l2reg=1e-6)
+    alpha = qml.math.cho_solve(kernel_train, properties_train, l2reg=l2reg)
 
     predictions = np.dot(kernel_test, alpha)
 
@@ -31,48 +38,186 @@ def score_kernel(kernel, properties, idxs_train, idxs_test):
     return rmse
 
 
-def cross_validation_score(kernel, properties, score_func=score_kernel, **kwargs):
+def score_rmse(kernel, properties, idxs_train, idxs_test, l2reg=1e-6):
 
-    fold_five = sklearn.model_selection.KFold(n_splits=5, random_state=42, shuffle=True)
-    n_items = kernel.shape[0]
-    X = list(range(n_items))
+    kernel_train = copy.deepcopy(kernel[np.ix_(idxs_train, idxs_train)])
+    kernel_test  = copy.deepcopy(kernel[np.ix_(idxs_test, idxs_train)])
 
-    scores = []
+    properties_train = properties[idxs_train]
+    properties_test = properties[idxs_test]
 
-    for idxs_train, idxs_test in fold_five.split(X):
+    alpha = qml.math.cho_solve(kernel_train, properties_train, l2reg=l2reg)
 
-        score = cross_validated_learning_curve(kernel, properties, idxs_train, idxs_test, **kwargs)
-        # score = score_func(kernel, properties, idxs_train, idxs_test)
-        scores.append(score)
+    predictions = np.dot(kernel_test, alpha)
 
-    scores = np.array(scores)
-    scores = scores.T
+    diff = predictions - properties_test
+    diff = diff**2
+    rmse = np.sqrt(diff.mean())
 
-    return scores
+    return rmse
 
 
-def cross_validated_learning_curve(kernel, properties, idxs_train, idxs_test,
-    n_trains=[2**x for x in range(4, 15)],
+def learning_curves(kernel, properties, idxs_train, idxs_test,
+    score_func=score_rmse,
+    training_points=None,
     check_len=True,
     **kwargs):
+    """
+    """
 
     n_items = len(idxs_train)
 
-    scores = []
+    if training_points is None:
+        training_points = [2**x for x in range(4, 15)]
 
-    for n in n_trains:
+    scores = []
+    for n in training_points:
 
         if n > n_items and check_len: break
 
         idxs = idxs_train[:n]
-
         score = score_kernel(kernel, properties, idxs, idxs_test, **kwargs)
         scores.append(score)
 
     return scores
 
 
-def learning_curves(scr):
+def cross_validation(kernels, properties,
+    score_func=score_rmse,
+    training_points=None):
+    """
+
+    iterate over kernels and select best index for each n_learn
+
+    """
+
+    fold_five = sklearn.model_selection.KFold(n_splits=5, random_state=45, shuffle=True)
+
+    n_points = len(training_points)
+
+    kernel_scores = []
+    mean_scores = []
+    winner_idxs = np.zeros(n_points, dtype=int)
+    winner_idxs -= 1
+    winner_mean = np.zeros(n_points)
+    winner_mean += np.inf
+
+    for idxk, kernel in enumerate(kernels):
+
+        n_items = kernel.shape[0]
+        X = list(range(n_items))
+
+        kernel_score = []
+
+        for idxs_train, idxs_test in fold_five.split(X):
+            training_scores = learning_curves(kernel, properties, idxs_train, idxs_test,
+                score_func=score_func,
+                training_points=training_points)
+
+            kernel_score.append(training_scores)
+            print(training_scores)
+
+        kernel_score = np.array(kernel_score)
+        kernel_score = kernel_score.T
+        kernel_scores.append(kernel_score)
+
+        mean_score = np.mean(kernel_score, axis=1)
+        print(mean_score)
+        print(winner_idxs)
+        print(winner_mean)
+
+        for i in range(n_points):
+            this_mean = mean_score[i]
+            that_mean = winner_mean[i]
+
+            if this_mean < that_mean:
+                winner_mean[i] = this_mean
+                winner_idxs[i] = idxk
+
+        print(winner_idxs)
+        print(winner_mean)
+
+
+    print()
+    print(winner_idxs)
+    print(winner_mean)
+
+    quit()
+
+    return idx_winners, scores
+
+
+def cross_validation_score(kernel, properties, score_func=score_kernel,
+    n_trains=[2**x for x in range(4, 15)],
+    parameters=None,
+    **kwargs):
+    """
+
+    """
+
+    # fold-it
+    fold_five = sklearn.model_selection.KFold(n_splits=5, random_state=45, shuffle=True)
+    n_items = kernel.shape[0]
+    X = list(range(n_items))
+
+    # for hp opt
+    l2regs = [10**-x for x in range(1,10,2)] + [0.0]
+
+    reg_winners = []
+    reg_scores = []
+
+    for l2reg in l2regs:
+
+        scores = []
+
+        for idxs_train, idxs_test in fold_five.split(X):
+
+            learn_score = cross_validated_learning_curve(kernel, properties, idxs_train, idxs_test,
+                l2reg=l2reg,
+                n_trains=n_trains,
+                **kwargs)
+            scores.append(learn_score)
+
+        scores = np.array(scores)
+        scores = scores.T
+
+        reg_scores.append(scores)
+
+        print(scores)
+        quit()
+
+    # for i in len():
+
+
+    quit()
+
+    return
+
+
+def cross_validated_learning_curve(kernel, properties, idxs_train, idxs_test,
+    n_trains=[2**x for x in range(4, 15)],
+    check_len=True,
+    l2reg=1e-6,
+    **kwargs):
+
+    n_items = len(idxs_train)
+
+    score_parameters = []
+
+    scores = []
+    for n in n_trains:
+
+        if n > n_items and check_len: break
+
+        idxs = idxs_train[:n]
+
+        score = score_kernel(kernel, properties, idxs, idxs_test, l2reg=l2reg, **kwargs)
+        scores.append(score)
+
+    return scores
+
+
+def dump_kernel_scores(scr):
 
     # Define n_training
     n_trains=[2**x for x in range(4, 12)]
@@ -83,20 +228,21 @@ def learning_curves(scr):
     properties = misc.load_npy(scr + "properties")
 
     # TODO Load done kernels:
-    names = ["fchl19", "fp"]
+    names = ["fp"]
     for name in names:
+        break
         kernel = misc.load_npy(scr + "kernel." + name)
-        scores = cross_validation_score(kernel, properties, n_trains=n_trains)
+        scores, parameters = cross_validation_score(kernel, properties, n_trains=n_trains)
         misc.save_npy(scr + "score."+name, scores)
         scores = np.around(np.mean(scores, axis=1), decimals=2)
         print(name, scores)
 
 
     # TODO Load multi kernels
-    names = ["fchl18"]
+    names = ["fchl19"]
     for name in names:
-        kernel = misc.load_npy(scr + "kernel." + name)[0]
-        scores = cross_validation_score(kernel, properties, n_trains=n_trains)
+        kernel = misc.load_npy(scr + "kernels." + name)
+        scores, parameters = cross_validation(kernel, properties, training_points=n_trains)
         misc.save_npy(scr + "score."+name, scores)
         scores = np.around(np.mean(scores, axis=1), decimals=2)
         print(name, scores)
@@ -198,8 +344,11 @@ def get_fchl19_kernels(reps, atoms, sigmas=None, return_sigmas=False):
     if sigmas is None:
         sigmas = [2**(i) for i in range(1,10)]
 
+
+    kernels = qml.kernels.get_local_symmetric_kernels(reps, atoms, sigmas)
+
     # kernel = qml.kernels.get_local_kernel(reps, reps, atoms, atoms, sigma)
-    kernels = qml.kernels.get_local_kernels(reps, reps, atoms, atoms, sigmas)
+    # kernels = qml.kernels.get_local_kernels(reps, reps, atoms, atoms, sigmas)
 
     if return_sigmas:
         return sigmas, kernels
@@ -347,7 +496,7 @@ def main():
         dump_distances_and_kernels(args.scratch)
 
     if args.get_learning_curves:
-        learning_curves(args.scratch)
+        dump_kernel_scores(args.scratch)
 
     return
 
